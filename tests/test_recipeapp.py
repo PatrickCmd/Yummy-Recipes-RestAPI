a16ee6598b4276c1ae560221c4bccb46e081d11c
PATRICK .Py <pwalukagga@gmail.com>
2017-10-04 18:25:17 +0300

    tests getting users

--- tests/test_recipeapp.py
+++ tests/test_recipeapp.py
@@ -16,19 +16,32 @@ class RecipeAppTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.user = json.dumps({"first_name": "Patrick",
                                 "last_name": "Walukagga",
-                                "email": "pwalukagga123456@gmail.com",
+                                "email": "pwalukagga@gmail.com",
                                 "password": "telnetcmd123"})
 
         # binds the app to the current context
         with self.app.app_context():
             # create all database tables
             db.create_all()
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
     
     def test_user_registration(self):
         '''Test API can register user (POST request)'''
         response = self.client().post('/auth/register', data=self.user)
         self.assertEqual(response.status_code, 201)
         self.assertIn('New user created!', str(response.data))
+    
+    def test_get_users(self):
+        '''Tests that users are return successfully'''
+        response = self.client().post('/auth/register', data=self.user)
+        response = self.client().get('/users')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
