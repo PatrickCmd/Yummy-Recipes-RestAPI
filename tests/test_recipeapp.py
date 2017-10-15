78411f4b6123a5430acaba3c40e5439dd1274978
PATRICK .Py <pwalukagga@gmail.com>
2017-10-15 21:33:57 +0300

    implements pagination of categories and recipes

--- tests/test_recipeapp.py
+++ tests/test_recipeapp.py
@@ -46,9 +46,9 @@ class RecipeAppTestCase(unittest.TestCase):
     def test_user_registration_fails_with_invalid_email(self):
         '''Test API can register user (POST request)'''
         user = json.dumps({"first_name": "Patrick",
-                                "last_name": "Walukagga",
-                                "email": "pwalukaggagmail.com",
-                                "password": "telnetcmd123"})
+                           "last_name": "Walukagga",
+                           "email": "pwalukaggagmail.com",
+                           "password": "telnetcmd123"})
         response = self.client().post('/auth/register', data=user)
         self.assertEqual(response.status_code, 200)
         self.assertIn('Invalid Email', str(response.data))
@@ -56,9 +56,9 @@ class RecipeAppTestCase(unittest.TestCase):
     def test_user_registration_fails_with_short_password(self):
         '''Test API can register user (POST request)'''
         user = json.dumps({"first_name": "Patrick",
-                                "last_name": "Walukagga",
-                                "email": "pwalukagga@gmail.com",
-                                "password": "teln"})
+                           "last_name": "Walukagga",
+                           "email": "pwalukagga@gmail.com",
+                           "password": "teln"})
         response = self.client().post('/auth/register', data=user)
         self.assertEqual(response.status_code, 200)
         self.assertIn('Password is too short', str(response.data))
@@ -66,9 +66,9 @@ class RecipeAppTestCase(unittest.TestCase):
     def test_user_registration_fails_with_empty_credintials(self):
         '''Test API can register user (POST request)'''
         user = json.dumps({"first_name": "",
-                                "last_name": "",
-                                "email": "",
-                                "password": ""})
+                           "last_name": "",
+                           "email": "",
+                           "password": ""})
         response = self.client().post('/auth/register', data=user)
         self.assertEqual(response.status_code, 200)
         self.assertIn('All fields must be filled', str(response.data))
@@ -77,9 +77,9 @@ class RecipeAppTestCase(unittest.TestCase):
         '''Test API can register user (POST request)'''
         response = self.client().post('/auth/register', data=self.user)
         user = json.dumps({"first_name": "Patrick",
-                                "last_name": "Walukagga",
-                                "email": "pwalukagga@gmail.com",
-                                "password": "telnetcmd123"})
+                           "last_name": "Walukagga",
+                           "email": "pwalukagga@gmail.com",
+                           "password": "telnetcmd123"})
         response = self.client().post('/auth/register', data=user)
         self.assertEqual(response.status_code, 200)
         self.assertIn('User already exists', str(response.data))
@@ -137,6 +137,44 @@ class RecipeAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('recipe categories', str(response.data))
         self.assertIn('How to make breakfast', str(response.data))
+    
+    def test_user_retrieves_recipe_categories_with_limit(self):
+        response = self.client().post('/auth/register', data=self.user)        
+        username = "pwalukagga@gmail.com" 
+        password = "telnetcmd123"
+        url = '/auth/login'
+        response_login = self.open_with_auth(url, username, password)
+        # getting token after login
+        token = json.loads(response_login.get_data().
+                           decode('utf-8'))['token']
+        # adding custom 'x-access-token' to request headers
+        headers = {"x-access-token": token, 
+                   "Content-Type": "application/json"}
+        category_data = json.dumps({"name": "Breakfast", 
+                                     "description": 
+                                     "How to make breakfast"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        category_data = json.dumps({"name": "Lunch", 
+                                     "description": 
+                                     "How to make lunch"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        category_data = json.dumps({"name": "Dinner", 
+                                     "description": 
+                                     "How to make Dinner"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        response = self.client().get('/recipe_category?limit=2', 
+                                      headers=headers,
+                                      data=category_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('recipe categories', str(response.data))
+        self.assertIn('How to make breakfast', str(response.data))
+        self.assertIn('How to make lunch', str(response.data))
     
     def test_user_get_recipe_category(self):
         response = self.client().post('/auth/register', data=self.user)        
@@ -436,6 +474,58 @@ class RecipeAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('Chicken Buffe', str(response.data))
         self.assertIn('Beef Buffe', str(response.data))
+    
+    def test_get_all_recipes_in_category_with_limit(self):
+        response = self.client().post('/auth/register', data=self.user)        
+        username = "pwalukagga@gmail.com" 
+        password = "telnetcmd123"
+        url = '/auth/login'
+        response_login = self.open_with_auth(url, username, password)
+        # getting token after login
+        token = json.loads(response_login.get_data().
+        decode('utf-8'))['token']
+        # adding custom 'x-access-token' to request headers
+        headers = {"x-access-token": token, 
+                   "Content-Type": "application/json"}
+        category_data = json.dumps({"name": "Breakfast", 
+                                     "description": 
+                                     "How to make breakfast"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        category_data = json.dumps({"name": "LunchBuffe", 
+                                     "description": 
+                                     "How to make lunch buffe"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        recipe_data = json.dumps({"name": "Chicken Buffe", 
+                                  "ingredients": "oil, Onions,\
+                                  Tomatoes",
+                                  "description": "Mix and boil"})
+        response = self.client().post('/recipe_category/2/recipes', 
+                                      headers=headers, 
+                                      data=recipe_data)
+        recipe_data = json.dumps({"name": "Beef Buffe", 
+                                  "ingredients": "oil, Onions,\
+                                  Tomatoes",
+                                  "description": "Mix and boil"})
+        response = self.client().post('/recipe_category/2/recipes', 
+                                      headers=headers, 
+                                      data=recipe_data)
+        recipe_data = json.dumps({"name": "Pork Buffe", 
+                                  "ingredients": "oil, Onions,\
+                                  Tomatoes",
+                                  "description": "Mix and boil"})
+        response = self.client().post('/recipe_category/2/recipes', 
+                                      headers=headers, 
+                                      data=recipe_data)
+        response = self.client().get('/recipe_category/2/recipes?limit=3', 
+                                      headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Chicken Buffe', str(response.data))
+        self.assertIn('Beef Buffe', str(response.data))
+        self.assertIn('Pork Buffe', str(response.data))
     
     def test_get_all_recipes_in_category_which_doesnot_exist(self):
         response = self.client().post('/auth/register', data=self.user)        
