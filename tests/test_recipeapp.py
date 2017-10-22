6dbcaebdcaae1b14b751803257802868ef114803
PATRICK .Py <pwalukagga@gmail.com>
2017-10-22 13:59:48 +0300

    validates check name has no special characters

--- tests/test_recipeapp.py
+++ tests/test_recipeapp.py
@@ -53,6 +53,17 @@ class RecipeAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('Invalid Email', str(response.data))
     
+    def test_user_registration_fails_with_name_having_special_characters(self):
+        '''Test API can register user (POST request)'''
+        user = json.dumps({"first_name": "Patrick@34&*%",
+                           "last_name": "Walukagga@#$%$!^@&",
+                           "email": "pwalukagga@gmail.com",
+                           "password": "telnet123"})
+        response = self.client().post('/auth/register', data=user)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Name must not contain any special \
+                               character', str(response.data))
+    
     def test_user_registration_fails_with_short_password(self):
         '''Test API can register user (POST request)'''
         user = json.dumps({"first_name": "Patrick",
@@ -175,6 +186,43 @@ class RecipeAppTestCase(unittest.TestCase):
         self.assertIn('recipe categories', str(response.data))
         self.assertIn('How to make breakfast', str(response.data))
         self.assertIn('How to make lunch', str(response.data))
+    
+    def test_user_retrieves_recipe_category_with_search(self):
+        response = self.client().post('/auth/register', data=self.user)        
+        username = "pwalukagga@gmail.com" 
+        password = "telnetcmd123"
+        url = '/auth/login'
+        response_login = self.open_with_auth(url, username, password)
+        # getting token after login
+        token = json.loads(response_login.get_data().
+                           decode('utf-8'))['token']
+        # adding custom 'x-access-token' to request headers
+        headers = {"x-access-token": token, 
+                   "Content-Type": "application/json"}
+        category_data = json.dumps({"name": "Breakfast", 
+                                     "description": 
+                                     "How to make breakfast"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        category_data = json.dumps({"name": "Lunch", 
+                                     "description": 
+                                     "How to make lunch"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        category_data = json.dumps({"name": "Dinner", 
+                                     "description": 
+                                     "How to make Dinner"})
+        response = self.client().post('/recipe_category', 
+                                      headers=headers,
+                                      data=category_data)
+        response = self.client().get('/recipe_category?q=Dinner', 
+                                      headers=headers,
+                                      data=category_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('recipe categories', str(response.data))
+        self.assertIn('How to make Dinner', str(response.data))
     
     def test_user_get_recipe_category(self):
         response = self.client().post('/auth/register', data=self.user)        
